13c1cf30cbf80fdf8f36f9c1c9bef591b60b7145
Valentin Ambroise <valentin.ambroise@outlook.com>
2022-07-28 00:38:12 +0200

    Add an event to send data saved in db but not available in Ouranos to Ouranos

--- src/events/__init__.py
+++ src/events/__init__.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import logging
 import typing as t
 
@@ -9,6 +10,12 @@ if t.TYPE_CHECKING:  # pragma: no cover
     from src.ecosystem import Ecosystem
 
 
+if Config.USE_DATABASE:
+    from sqlalchemy import select
+
+    from src.database import SQLAlchemyWrapper
+    from src.database.models import SensorHistory
+
 logger = logging.getLogger(f"{Config.APP_NAME.lower()}.broker")
 
 
@@ -16,11 +23,16 @@ class Events:
     """A class holding all the events coming from either socketio or
     event-dispatcher
 
-    : param ecosystem_dict: a dict holding all the Ecosystem instances
+    :param ecosystem_dict: a dict holding all the Ecosystem instances
     """
     def __init__(self, ecosystem_dict: dict[str, "Ecosystem"]) -> None:
         self.ecosystems = ecosystem_dict
         self._registered = False
+        if Config.USE_DATABASE:
+            from src.database import SQLAlchemyWrapper
+            self.db = SQLAlchemyWrapper(Config)
+        else:
+            self.db = None
 
     def emit(self, *args, **kwargs):
         raise NotImplementedError(
@@ -58,11 +70,10 @@ class Events:
         self.emit("pong", data=pong)
 
     def _get_uid_list(self, ecosystem_uids: t.Union[str, tuple] = "all") -> list:
+        if isinstance(ecosystem_uids, str):
+            ecosystem_uids = ecosystem_uids.split(",")
         if "all" in ecosystem_uids:
             return [e_uid for e_uid in self.ecosystems.keys()]
-        if isinstance(ecosystem_uids, str):
-            return [ecosystem_uids] \
-                if ecosystem_uids in self.ecosystems.keys() else []
         else:
             return [e_uid for e_uid in ecosystem_uids
                     if e_uid in self.ecosystems.keys()]
@@ -124,9 +135,9 @@ class Events:
 
     def on_turn_light(self, message: dict) -> None:
         logger.debug("Received turn_light event")
-        ecosystem_uid = message["ecosystem"]
-        mode = message["mode"]
-        countdown = message.get("countdown", 0)
+        ecosystem_uid: str = message["ecosystem"]
+        mode: str = message["mode"]
+        countdown: float = message.get("countdown", 0)
         try:
             self.ecosystems[ecosystem_uid].turn_actuator(
                 "light", mode=mode, countdown=countdown
@@ -138,10 +149,10 @@ class Events:
 
     def on_turn_actuator(self, message: dict) -> None:
         logger.debug("Received turn_actuator event")
-        ecosystem_uid = message["ecosystem"]
-        actuator = message["actuator"]
-        mode = message["mode"]
-        countdown = message.get("countdown", 0)
+        ecosystem_uid: str = message["ecosystem"]
+        actuator: str = message["actuator"]
+        mode: str = message["mode"]
+        countdown: float = message.get("countdown", 0.0)
         try:
             self.ecosystems[ecosystem_uid].turn_actuator(
                 actuator=actuator, mode=mode, countdown=countdown
@@ -154,9 +165,9 @@ class Events:
                 self.on_send_light_data(ecosystem_uid)
 
     def on_change_management(self, message: dict) -> None:
-        ecosystem_uid = message["ecosystem"]
-        management = message["management"]
-        status = message["status"]
+        ecosystem_uid: str = message["ecosystem"]
+        management: str = message["management"]
+        status: bool = message["status"]
         try:
             self.ecosystems[ecosystem_uid].config.set_management(management, status)
             self.ecosystems[ecosystem_uid].config.save()
@@ -164,3 +175,25 @@ class Events:
         except KeyError:
             print(f"{ecosystem_uid}'s management {management} cannot be turned "
                   f"to {status} yet")
+
+    def on_get_data_since(self, message: dict) -> None:
+        if not self.db:
+            logger.error(
+                "Received 'get_data_since' event but USE_DATABASE is set to False"
+            )
+            return
+        ecosystem_uids: str = message["ecosystems"]
+        uids: list = self._get_uid_list(ecosystem_uids)
+        since_str: str = message["since"]
+        since: datetime = datetime.fromisoformat(since_str).astimezone()
+        with self.db.scopped_session() as session:
+            query = (
+                select(SensorHistory)
+                    .where(SensorHistory.datetime >= since)
+                    .where(SensorHistory.ecosystem_uid.in_(uids))
+            )
+            results = session.execute(query).all().scalars()
+        self.emit(
+            "sensor_data_record",
+            [result.dict_repr for result in results]
+        )
