9a015908cd56fec7350de7c65b2ed8738a3a1ab2
Valentin Ambroise <valentin.ambroise@outlook.com>
2023-06-30 01:27:35 +0200

    Bump to version 0.6.0
    * Major changes:
    
    - Added various `pydantic` validators to validate data from `*.cfg` files and from events. The validators common with Ouranos are found in the `gaia_validators` package
    
    - Added events for "CRUD actions" order coming from Ouranos
    
    - Reworked `config.environments`. Renamed it `config.from_files`. Renamed `GeneralConfig` to `EngineConfig` and `SpecificConfig` to `EcosystemConfig`. All their methods and properties are safer thanks to data validation. `SpecificConfig` now returns singletons based on ecosystem uid
    
    - `Hardware` now returns singletons based on hardware uid. It allows to reduce cross-subroutines error-prone calls.
    
    - Added a `ActuatorHandler` to handle actuators states. All the actuators states are stored at the ecosystem level
    
    - Create a `gaia.service` file usable with `systemd` upon install

--- src/gaia/__init__.py
+++ src/gaia/__init__.py
@@ -1,6 +1,6 @@
-__version__ = "0.5.3"
+__version__ = "0.6.0"
 
-from gaia.config import get_base_dir, get_config
+from gaia.config import EcosystemConfig, EngineConfig, get_base_dir, get_config
 from gaia.ecosystem import Ecosystem
 from gaia.engine import Engine
 from gaia.main import Gaia, main
