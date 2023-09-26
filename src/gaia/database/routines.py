ebfbdaac43579cb86306d80245194aeabd80bb24
Valentin Ambroise <valentin.ambroise@outlook.com>
2023-09-26 21:22:34 +0200

    Improve `SensorBuffer.get_buffered_data`

--- src/gaia/database/routines.py
+++ src/gaia/database/routines.py
@@ -1,5 +1,8 @@
 from datetime import datetime, timezone
 import typing as t
+from typing import Callable
+
+from sqlalchemy.orm import scoped_session, Session
 
 from gaia_validators import SensorsData
 
@@ -8,16 +11,18 @@ from gaia.database.models import SensorBuffer, SensorRecord
 
 
 if t.TYPE_CHECKING:
-    from sqlalchemy.orm import scoped_session
-
     from gaia.engine import Engine
 
 
 sensors_logging_period = get_config().SENSORS_LOGGING_PERIOD
 
 
-def log_sensors_data(scoped_session: "scoped_session", engine: "Engine"):
-    with scoped_session() as session:
+def log_sensors_data(
+        scoped_session_: Callable[..., scoped_session],
+        engine: "Engine"
+) -> None:
+    with scoped_session_() as session:
+        session: Session
         for ecosystem_uid, ecosystem in engine.ecosystems.items():
             sensors_data = ecosystem.sensors_data
             database_management = ecosystem.config.get_management("database")
