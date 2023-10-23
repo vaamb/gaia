46c89275f4dd723c4d63a636ccb357ced14dd87e
Valentin Ambroise <valentin.ambroise@outlook.com>
2023-10-23 20:18:26 +0200

    Move data from `utils.py` to `data.py`
     - This currently makes `utils.py` empty

---
+++ tests/data.py
@@ -0,0 +1,64 @@
+from datetime import time
+
+import gaia_validators as gv
+
+
+ecosystem_uid = "Rfrg5Kiv"
+ecosystem_name = "Testing ecosystem"
+
+i2c_address = "I2C_default"
+gpio_address = "GPIO_4:BOARD_12"
+
+hardware_uid = "cpgCZFJGGYlIXlLL"
+hardware_name = "TestingHardware"
+hardware_address = "GPIO_10"
+
+hardware_info = {
+    "name": hardware_name,
+    "address": hardware_address,
+    "model": "DHT22",
+    "type": "sensor",
+    "level": "plants",
+    "measures": ["testMeasure"],
+    "plants": ["testPlant"],
+}
+
+sun_times = {
+    "twilight_begin": time(6, 15),
+    "sunrise": time(7, 0),
+    "sunset": time(20, 0),
+    "twilight_end": time(20, 45),
+}
+
+ecosystem_info = {
+    ecosystem_uid: {
+        "name": ecosystem_name,
+        "status": False,
+        "management": {
+            "sensors": False,
+            "light": False,
+            "climate": False,
+            "watering": False,
+            "health": False,
+            "alarms": False,
+            "pictures": False,
+            "database": False,
+        },
+        "environment": {
+            "chaos": {
+                "frequency": 0,
+                "duration": 0,
+                "intensity": 0.0,
+            },
+            "sky": {
+                "day": time(8,00),
+                "night": time(20, 00),
+                "lighting": gv.LightMethod.fixed,
+            },
+            "climate": {},
+        },
+        "IO": {
+            hardware_uid: hardware_info
+        },
+    },
+}
