44387802eae5fa5d53bc43ae4d856c49345afdca
Valentin Ambroise <valentin.ambroise@outlook.com>
2023-10-23 20:33:17 +0200

    Add a test for the relationships between `EngineConfig`, `Engine`, `EcosystemConfig` and `Ecosystem`
    - Tests fixture and for the proxies between these classes
    
    - Remove `test_0_fixtures.py`

---
+++ tests/test_relationship.py
@@ -0,0 +1,11 @@
+from weakref import proxy
+
+
+def test_relationship(engine_config, engine, ecosystem_config, ecosystem):
+    assert engine_config.engine is proxy(engine)
+    assert engine.config is engine_config
+
+    assert ecosystem_config.general is proxy(engine_config)
+
+    assert ecosystem.engine is proxy(engine)
+    assert ecosystem.config is ecosystem_config
