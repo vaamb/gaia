8eac9a6f7c91962eb623ce7efb76c8f3f67286da
Valentin Ambroise <113367796+vaamb@users.noreply.github.com>
2023-11-09 22:52:24 +0100

    Fix a typo in config's `__init__.py` (#47)

--- src/gaia/config/__init__.py
+++ src/gaia/config/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from gaia.config._utils import (
-    configure_logging, GaiaConfig, get_config,set_config)
+    configure_logging, GaiaConfig, get_config, set_config)
 from gaia.config.base import BaseConfig
 from gaia.config.from_files import (
     CacheType, ConfigType, EcosystemConfig, EngineConfig, get_IDs as get_ecosystem_IDs)
