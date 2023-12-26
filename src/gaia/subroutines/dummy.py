ef2432c74b3f79bad2ff37827c42d97fcb8c96f9
Valentin Ambroise <valentin.ambroise@outlook.com>
2023-12-26 16:40:25 +0100

    Easily toggle `Dummy` subroutine management

--- src/gaia/subroutines/dummy.py
+++ src/gaia/subroutines/dummy.py
@@ -2,8 +2,10 @@ from gaia.subroutines.template import SubroutineTemplate
 
 
 class Dummy(SubroutineTemplate):
+    manageable_state = True
+
     def _compute_if_manageable(self) -> bool:
-        return True
+        return self.manageable_state
 
     def _start(self) -> None:
         pass
