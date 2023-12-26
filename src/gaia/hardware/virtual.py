68a875641dbe02d15ee058e12e8bf156525aaa45
Valentin Ambroise <valentin.ambroise@outlook.com>
2023-12-26 15:56:49 +0100

    Fix a cyclic import

--- src/gaia/hardware/virtual.py
+++ src/gaia/hardware/virtual.py
@@ -1,4 +1,3 @@
-from gaia import get_config
 from gaia.hardware.abc import Hardware
 from gaia.virtual import get_virtual_ecosystem
 
@@ -6,5 +5,6 @@ from gaia.virtual import get_virtual_ecosystem
 class virtualHardware(Hardware):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        from gaia import get_config
         if get_config().VIRTUALIZATION:
             get_virtual_ecosystem(self.subroutine.ecosystem.uid, start=True)
