vaamb/gaia

commit a053bf5fa35655878ec56abd658a502a5cfea448

Import `gaia_validators` as `gv` instead of importing its models (#58)
- Make cleaner import section and allows to easily distinguish internal models from the one coming from `gaia_validators`

src/gaia/hardware/abc.py
@@ -12,9 +12,7 @@ from typing import Any, Literal, Self, Type
 import weakref
 from weakref import WeakValueDictionary
 
-from gaia_validators import (
-    safe_enum_from_name, HardwareConfig, HardwareLevel, HardwareLevelNames,
-    HardwareType, HardwareTypeNames, SensorRecord)
+import gaia_validators as gv
 
 from gaia.dependencies.camera import check_dependencies, Image
 from gaia.hardware.multiplexers import Multiplexer, multiplexer_models

src/gaia/hardware/sensors/abc.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from gaia_validators import SensorRecord
+import gaia_validators as gv
 
 from gaia.hardware.abc import BaseSensor
 from gaia.utils import (
@@ -13,11 +13,11 @@ class TempHumSensor(BaseSensor):
             "This method must be implemented in a subclass"
         )
 
-    def get_data(self) -> list[SensorRecord]:
+    def get_data(self) -> list[gv.SensorRecord]:
         raw_humidity, raw_temperature = self._get_raw_data()
         data = []
         if "humidity" in self.measures:
-            data.append(SensorRecord(
+            data.append(gv.SensorRecord(
                 sensor_uid=self.uid,
                 measure="humidity",
                 value=raw_humidity
@@ -26,7 +26,7 @@ class TempHumSensor(BaseSensor):
         if "temperature" in self.measures:
             temperature = temperature_converter(
                 raw_temperature, "celsius", get_unit("temperature", "celsius"))
-            data.append(SensorRecord(
+            data.append(gv.SensorRecord(
                 sensor_uid=self.uid,
                 measure="temperature",
                 value=temperature
@@ -36,14 +36,14 @@ class TempHumSensor(BaseSensor):
             raw_dew_point = get_dew_point(raw_temperature, raw_humidity)
             dew_point = temperature_converter(
                 raw_dew_point, "celsius", get_unit("temperature", "celsius"))
-            data.append(SensorRecord(
+            data.append(gv.SensorRecord(
                 sensor_uid=self.uid,
                 measure="dew_point",
                 value=dew_point
             ))
 
         if "absolute_humidity" in self.measures:
-            data.append(SensorRecord(
+            data.append(gv.SensorRecord(
                 sensor_uid=self.uid,
                 measure="absolute_humidity",
                 value=get_absolute_humidity(raw_temperature, raw_humidity)

src/gaia/database/models.py
@@ -8,7 +8,7 @@ import sqlalchemy as sa
 from sqlalchemy import delete, select, update
 from sqlalchemy.orm import Mapped, mapped_column, Session
 
-from gaia_validators import BufferedSensorsDataPayload, BufferedSensorRecord
+import gaia_validators as gv
 from sqlalchemy_wrapper import SQLAlchemyWrapper
 
 from gaia.utils import json
@@ -64,7 +64,7 @@ class SensorBuffer(BaseSensorRecord):
             cls,
             session: Session,
             per_page: int = 50
-    ) -> Generator[BufferedSensorsDataPayload]:
+    ) -> Generator[gv.BufferedSensorsDataPayload]:
         page: int = 0
         try:
             while True:
@@ -79,11 +79,11 @@ class SensorBuffer(BaseSensorRecord):
                 if not buffered_data:
                     break
                 uuid = uuid4()
-                rv: list[BufferedSensorRecord] = []
+                rv: list[gv.BufferedSensorRecord] = []
                 for data in buffered_data:
                     data.exchange_uuid = uuid
                     rv.append(
-                        BufferedSensorRecord(
+                        gv.BufferedSensorRecord(
                             ecosystem_uid=data.ecosystem_uid,
                             sensor_uid=data.sensor_uid,
                             measure=data.measure,
@@ -92,7 +92,7 @@ class SensorBuffer(BaseSensorRecord):
                         )
                     )
 
-                yield BufferedSensorsDataPayload(
+                yield gv.BufferedSensorsDataPayload(
                     data=rv,
                     uuid=uuid,
                 )

src/gaia/database/routines.py
@@ -4,7 +4,7 @@ from typing import Callable
 
 from sqlalchemy.orm import scoped_session, Session
 
-from gaia_validators import SensorsData
+import gaia_validators as gv
 
 from gaia.database.models import SensorBuffer, SensorRecord
 
@@ -23,7 +23,7 @@ def log_sensors_data(
         for ecosystem_uid, ecosystem in engine.ecosystems.items():
             sensors_data = ecosystem.sensors_data
             database_management = ecosystem.config.get_management("database")
-            if isinstance(sensors_data, SensorsData) and database_management:
+            if isinstance(sensors_data, gv.SensorsData) and database_management:
                 timestamp: datetime = sensors_data.timestamp
                 timestamp = timestamp.astimezone(timezone.utc)
                 if timestamp.minute % sensors_logging_period == 0:

src/gaia/hardware/sensors/I2C.py
@@ -4,7 +4,7 @@ from time import sleep
 import typing as t
 from typing import Type
 
-from gaia_validators import SensorRecord
+import gaia_validators as gv
 
 from gaia.hardware.abc import (
     BaseSensor, hardware_logger, i2cSensor, LightSensor, PlantLevelHardware)
@@ -101,26 +101,26 @@ class ENS160(i2cSensor):
         self.device.temperature_compensation = temperature
         self.device.humidity_compensation = humidity
 
-    def get_data(self) -> list[SensorRecord]:
+    def get_data(self) -> list[gv.SensorRecord]:
         # TODO: access temperature and humidity data to compensate
         data = []
         AQI, eCO2, TVOC = self._get_raw_data()
         if "AQI" in self.measures:
-            data.append(SensorRecord(
+            data.append(gv.SensorRecord(
                 sensor_uid=self.uid,
                 measure="AQI",
                 value=AQI
             ))
 
         if "eCO2" in self.measures:
-            data.append(SensorRecord(
+            data.append(gv.SensorRecord(
                 sensor_uid=self.uid,
                 measure="eCO2",
                 value=eCO2
             ))
 
         if "TVOC" in self.measures:
-            data.append(SensorRecord(
+            data.append(gv.SensorRecord(
                 sensor_uid=self.uid,
                 measure="TVOC",
                 value=TVOC
@@ -158,10 +158,10 @@ class VEML7700(i2cSensor, LightSensor):
             )
             return None
 
-    def get_data(self) -> list[SensorRecord]:
+    def get_data(self) -> list[gv.SensorRecord]:
         data = []
         if "lux" in self.measures or "light" in self.measures:
-            data.append(SensorRecord(
+            data.append(gv.SensorRecord(
                 sensor_uid=self.uid,
                 measure="light",
                 value=self.get_lux()
@@ -199,10 +199,10 @@ class VCNL4040(i2cSensor, LightSensor):
             )
             return None
 
-    def get_data(self) -> list[SensorRecord]:
+    def get_data(self) -> list[gv.SensorRecord]:
         data = []
         if "lux" in self.measures or "light" in self.measures:
-            data.append(SensorRecord(
+            data.append(gv.SensorRecord(
                 sensor_uid=self.uid,
                 measure="light",
                 value=self.get_lux()
@@ -229,7 +229,7 @@ class CapacitiveSensor(i2cSensor):
             from gaia.hardware._compatibility import Seesaw
         return Seesaw(self._get_i2c(), self._address_book.primary.main)
 
-    def get_data(self) -> list[SensorRecord]:
+    def get_data(self) -> list[gv.SensorRecord]:
         raise NotImplementedError(
             "This method must be implemented in a subclass"
         )
@@ -263,14 +263,14 @@ class CapacitiveMoisture(CapacitiveSensor, PlantLevelHardware):
                 break
         return moisture, temperature
 
-    def get_data(self) -> list[SensorRecord]:
+    def get_data(self) -> list[gv.SensorRecord]:
         try:
             moisture, raw_temperature = self._get_raw_data()
         except RuntimeError:
             moisture = raw_temperature = None
         data = []
         if "moisture" in self.measures:
-            data.append(SensorRecord(
+            data.append(gv.SensorRecord(
                 sensor_uid=self.uid,
                 measure="moisture",
                 value=moisture
@@ -279,7 +279,7 @@ class CapacitiveMoisture(CapacitiveSensor, PlantLevelHardware):
         if "temperature" in self.measures:
             temperature = temperature_converter(
                 raw_temperature, "celsius", get_unit("temperature", "celsius"))
-            data.append(SensorRecord(
+            data.append(gv.SensorRecord(
                 sensor_uid=self.uid,
                 measure="temperature",
                 value=temperature

src/gaia/subroutines/health.py
@@ -4,8 +4,7 @@ from datetime import datetime
 import io
 import typing as t
 
-from gaia_validators import (
-    ActuatorMode, ActuatorModePayload, Empty, HealthRecord)
+import gaia_validators as gv
 
 from gaia.dependencies import check_dependencies
 from gaia.hardware import camera_models
@@ -24,7 +23,7 @@ class Health(SubroutineTemplate):
         super().__init__(*args, **kwargs)
         self.hardware_choices = camera_models
         self.hardware: dict[str, Camera]
-        self._plants_health: HealthRecord | Empty = Empty()
+        self._plants_health: gv.HealthRecord | gv.Empty = gv.Empty()
         self._imageIO = io.BytesIO()
         self._finish__init__()
 
@@ -51,7 +50,7 @@ class Health(SubroutineTemplate):
             green = random.randrange(12000, 1500000, 1000)
             necrosis = random.uniform(5, 55)
             health_index = random.uniform(70, 97)
-            self._plants_health = HealthRecord(
+            self._plants_health = gv.HealthRecord(
                 timestamp=datetime.now().astimezone().replace(microsecond=0),
                 green=green,
                 necrosis=round(necrosis, 2),
@@ -70,15 +69,15 @@ class Health(SubroutineTemplate):
             light_subroutine: "Light" = self.ecosystem.subroutines["light"]
             light_mode = light_subroutine.actuator_handler.mode
             light_status = light_subroutine.actuator_handler.status
-            light_subroutine.turn_light(ActuatorModePayload.on)
+            light_subroutine.turn_light(gv.ActuatorModePayload.on)
             self.take_picture()
-            if light_mode is ActuatorMode.automatic:
-                light_subroutine.turn_light(ActuatorModePayload.automatic)
+            if light_mode is gv.ActuatorMode.automatic:
+                light_subroutine.turn_light(gv.ActuatorModePayload.automatic)
             else:
                 if light_status:
-                    light_subroutine.turn_light(ActuatorModePayload.on)
+                    light_subroutine.turn_light(gv.ActuatorModePayload.on)
                 else:
-                    light_subroutine.turn_light(ActuatorModePayload.off)
+                    light_subroutine.turn_light(gv.ActuatorModePayload.off)
         else:
             self.take_picture()
         self.analyse_picture()
@@ -115,5 +114,5 @@ class Health(SubroutineTemplate):
         pass
 
     @property
-    def plants_health(self) -> HealthRecord | Empty:
+    def plants_health(self) -> gv.HealthRecord | gv.Empty:
         return self._plants_health

src/gaia/subroutines/sensors.py
@@ -6,8 +6,7 @@ from threading import Event, Lock, Thread
 from time import monotonic
 import typing as t
 
-from gaia_validators import (
-    Empty, HardwareConfig, MeasureAverage, SensorsData, SensorsDataDict)
+import gaia_validators as gv
 
 from gaia.hardware import sensor_models
 from gaia.hardware.abc import BaseSensor
@@ -27,7 +26,7 @@ class Sensors(SubroutineTemplate):
         self._stop_event = Event()
         self._loop_timeout: float = float(
             self.ecosystem.engine.config.app_config.SENSORS_TIMEOUT)
-        self._sensors_data: SensorsData | Empty = Empty()
+        self._sensors_data: gv.SensorsData | gv.Empty = gv.Empty()
         self._data_lock = Lock()
         self._finish__init__()
 
@@ -88,7 +87,7 @@ class Sensors(SubroutineTemplate):
     def thread(self, thread: Thread | None) -> None:
         self._thread = thread
 
-    def add_hardware(self, hardware_config: HardwareConfig) -> BaseSensor:
+    def add_hardware(self, hardware_config: gv.HardwareConfig) -> BaseSensor:
         model = hardware_config.model
         if self.ecosystem.engine.config.app_config.VIRTUALIZATION:
             if not model.startswith("virtual"):
@@ -112,7 +111,7 @@ class Sensors(SubroutineTemplate):
             raise RuntimeError(
                 "Sensors subroutine has to be started to update the sensors data"
             )
-        cache: SensorsDataDict = {}
+        cache: gv.SensorsDataDict = {}
         to_average: dict[str, list[float]] = {}
         cache["timestamp"] = datetime.now(timezone.utc).replace(microsecond=0)
         cache["records"] = []
@@ -132,7 +131,7 @@ class Sensors(SubroutineTemplate):
             except KeyError:
                 to_average[record.measure] = [record.value]
         cache["average"] = [
-            MeasureAverage(
+            gv.MeasureAverage(
                 measure=measure,
                 value=round(mean(value), 2),
                 timestamp=None
@@ -140,16 +139,16 @@ class Sensors(SubroutineTemplate):
             for measure, value in to_average.items()
         ]
         if len(cache["records"]) > 0:
-            self.sensors_data = SensorsData(**cache)
+            self.sensors_data = gv.SensorsData(**cache)
         else:
-            self.sensors_data = Empty()
+            self.sensors_data = gv.Empty()
 
     @property
-    def sensors_data(self) -> SensorsData | Empty:
+    def sensors_data(self) -> gv.SensorsData | gv.Empty:
         with self._data_lock:
             return self._sensors_data
 
     @sensors_data.setter
-    def sensors_data(self, data: SensorsData | Empty) -> None:
+    def sensors_data(self, data: gv.SensorsData | gv.Empty) -> None:
         with self._data_lock:
             self._sensors_data = data

src/gaia/subroutines/template.py
@@ -7,7 +7,7 @@ import typing as t
 from typing import Type
 import weakref
 
-from gaia_validators import HardwareConfig
+import gaia_validators as gv
 
 from gaia.exceptions import HardwareNotFound
 from gaia.hardware.abc import BaseSensor, Camera, Dimmer, Hardware, Switch
@@ -117,7 +117,7 @@ class SubroutineTemplate(ABC):
 
     def add_hardware(
             self,
-            hardware_config: HardwareConfig,
+            hardware_config: gv.HardwareConfig,
     ) -> BaseSensor | Camera | Dimmer | Hardware | Switch | None:
         if not self.hardware_choices:
             raise RuntimeError("No 'hardware_choices' available.")

commit 3e00ef094bb7c1bb71e9f428c154dbe8a8bf5142

Fix an error in `hardware.abc.py` introduced by #58

src/gaia/hardware/abc.py
@@ -13,6 +13,7 @@ import weakref
 from weakref import WeakValueDictionary
 
 import gaia_validators as gv
+from gaia_validators import safe_enum_from_name
 
 from gaia.dependencies.camera import check_dependencies, Image
 from gaia.hardware.multiplexers import Multiplexer, multiplexer_models
@@ -209,8 +210,8 @@ class Hardware(metaclass=_MetaHardware):
             subroutine: "SubroutineTemplate" | None,
             uid: str,
             address: str,
-            level: HardwareLevel | HardwareLevelNames,
-            type: HardwareType | HardwareTypeNames,
+            level: gv.HardwareLevel | gv.HardwareLevelNames,
+            type: gv.HardwareType | gv.HardwareTypeNames,
             model: str,
             name: str | None = None,
             measures: list | None = None,
@@ -223,8 +224,8 @@ class Hardware(metaclass=_MetaHardware):
         else:
             self._subroutine = weakref.proxy(subroutine)
         self._uid: str = uid
-        self._level: HardwareLevel = safe_enum_from_name(HardwareLevel, level)
-        self._type: HardwareType = safe_enum_from_name(HardwareType, type)
+        self._level: gv.HardwareLevel = safe_enum_from_name(gv.HardwareLevel, level)
+        self._type: gv.HardwareType = safe_enum_from_name(gv.HardwareType, type)
         self._model: str = model
         self._name: str = name or uid
         address_list: list = address.split(":")
@@ -257,7 +258,7 @@ class Hardware(metaclass=_MetaHardware):
     @classmethod
     def from_hardware_config(
             cls,
-            hardware_config: HardwareConfig,
+            hardware_config: gv.HardwareConfig,
             subroutine: "SubroutineTemplate" | None
     ) -> Self:
         return cls(
@@ -312,11 +313,11 @@ class Hardware(metaclass=_MetaHardware):
         return self._model
 
     @property
-    def level(self) -> HardwareLevel:
+    def level(self) -> gv.HardwareLevel:
         return self._level
 
     @property
-    def type(self) -> HardwareType:
+    def type(self) -> gv.HardwareType:
         return self._type
 
     @property
@@ -476,7 +477,7 @@ class i2cHardware(Hardware):
 
 class PlantLevelHardware(Hardware):
     def __init__(self, *args, **kwargs) -> None:
-        kwargs["level"] = HardwareLevel.plants
+        kwargs["level"] = gv.HardwareLevel.plants
         super().__init__(*args, **kwargs)
         if not self.plants:  # pragma: no cover
             hardware_logger.warning(
@@ -487,7 +488,7 @@ class PlantLevelHardware(Hardware):
 
 class BaseSensor(Hardware):
     def __init__(self, *args, **kwargs) -> None:
-        kwargs["type"] = HardwareType.sensor
+        kwargs["type"] = gv.HardwareType.sensor
         super().__init__(*args, **kwargs)
         self.device: Any = self._get_device()
 
@@ -496,7 +497,7 @@ class BaseSensor(Hardware):
             "This method must be implemented in a subclass"
         )
 
-    def get_data(self) -> list[SensorRecord]:
+    def get_data(self) -> list[gv.SensorRecord]:
         raise NotImplementedError(
             "This method must be implemented in a subclass"
         )
@@ -508,14 +509,14 @@ class LightSensor(BaseSensor):
             "This method must be implemented in a subclass"
         )
 
-    def get_data(self) -> list[SensorRecord]:
+    def get_data(self) -> list[gv.SensorRecord]:
         raise NotImplementedError(
             "This method must be implemented in a subclass"
         )
 
 
 class gpioSensor(BaseSensor, gpioHardware):
-    def get_data(self) -> list[SensorRecord]:
+    def get_data(self) -> list[gv.SensorRecord]:
         raise NotImplementedError(
             "This method must be implemented in a subclass"
         )
@@ -529,7 +530,7 @@ class i2cSensor(BaseSensor, i2cHardware):
                 kwargs["address"] = f"I2C_{hex(default_address)}"
         super().__init__(*args, **kwargs)
 
-    def get_data(self) -> list[SensorRecord]:
+    def get_data(self) -> list[gv.SensorRecord]:
         raise NotImplementedError(
             "This method must be implemented in a subclass"
         )

tests/test_hardware.py
@@ -55,6 +55,7 @@ def test_hardware_creation_success(ecosystem_config: EcosystemConfig):
     }
     ecosystem_config.create_new_hardware(**valid_hardware_info)
 
+
 def test_hardware_update_fail_not_found(ecosystem_config: EcosystemConfig):
     with pytest.raises(HardwareNotFound):
         ecosystem_config.update_hardware("invalid_uid", {"address": "GPIO_7"})