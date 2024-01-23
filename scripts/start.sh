31b64e65e465f8a0f547e55da2e5e1f5af2c1096
Valentin Ambroise <113367796+vaamb@users.noreply.github.com>
2024-01-23 20:27:05 +0100

    Improve ´start.sh´ (#86)
    - Add the variable `GAIA_VENV_PATH` to set the path to the python venv used
    
    - Fix an issue where gaia would be called twice

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
+if [ -z ${GAIA_DIR+x} ];
+  then echo "\$GAIA_DIR is not set, exiting." exit;
+fi
+
 exec </dev/null >"$GAIA_DIR/logs/stdout" 2>&1
 
 trap '' HUP
 
-if pgrep -x "gaia" > /dev/null; then
-  echo "An instance of Gaia is already running, this might lead to conflicts"
+if pgrep -x "gaia" > /dev/null;
+  then
+    echo "An instance of Gaia is already running, this might lead to conflicts";
 fi
 
 echo "Starting Gaia";
-cd "$GAIA_DIR" || echo "\$GAIA_DIR is not set, exiting" exit
-source $GAIA_DIR/python_venv/bin/activate
-python3 $GAIA_DIR/main.py
+if [ -z ${GAIA_VENV_PATH+x} ];
+  then
+    cd "$GAIA_DIR" || echo "$GAIA_DIR does not exist, exiting" exit;
+    source $GAIA_DIR/python_venv/bin/activate;
+  else
+    source $GAIA_VENV_PATH;
+fi
 python3 -m gaia
