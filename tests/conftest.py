8a36b1b854a4f2595e4e9d2ebf716d2179ad1615
Valentin Ambroise <valentin.ambroise@outlook.com>
2024-02-12 00:17:10 +0100

    Add the crud test `test_wrong_routing`

--- tests/conftest.py
+++ tests/conftest.py
@@ -17,7 +17,8 @@ from gaia.subroutines import (
     Climate, Light, Sensors, subroutine_dict, subroutine_names)
 from gaia.utils import SingletonMeta, yaml
 
-from .data import ecosystem_info, ecosystem_name, light_info, light_uid
+from .data import (
+    ecosystem_info, ecosystem_name, engine_uid, light_info, light_uid)
 from .subroutines.dummy_subroutine import Dummy
 from .utils import get_logs_content
 
@@ -83,6 +84,7 @@ def testing_cfg(temp_dir) -> None:
         DIR = temp_dir
         AGGREGATOR_COMMUNICATION_URL = "memory:///"
         CONFIG_WATCHER_PERIOD = 100
+        ENGINE_UID = engine_uid
 
     GaiaConfigHelper.set_config(Config)
 
---
+++ tests/events/crud.py
@@ -0,0 +1,21 @@
+import gaia_validators as gv
+
+from gaia.events import Events
+
+from ..data import ecosystem_uid, engine_uid
+from ..utils import get_logs_content
+
+
+def test_wrong_routing(events_handler: Events):
+    # Wrong engine_uid
+    message = gv.CrudPayloadDict = gv.CrudPayload(
+        routing={"engine_uid": "wrong_uid", "ecosystem_uid": ecosystem_uid},
+        action=gv.CrudAction.create,
+        target="hardware",
+        data={},
+    ).model_dump()
+
+    events_handler.on_crud(message)
+
+    with get_logs_content(events_handler.engine.config.logs_dir / "gaia.log") as logs:
+        assert "Received 'on_crud' event intended to engine" in logs
