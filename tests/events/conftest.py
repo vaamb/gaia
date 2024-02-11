5185393754ed1bfed2cf7736464e914aa12e3a31
Valentin Ambroise <valentin.ambroise@outlook.com>
2024-02-12 00:16:34 +0100

    Add a mock dispatcher

---
+++ tests/events/conftest.py
@@ -0,0 +1,25 @@
+import pytest
+
+from gaia import Engine
+from gaia.events import Events
+
+from ..utils import MockDispatcher
+
+
+@pytest.fixture(scope="module")
+def mock_dispatcher():
+    mock_dispatcher = MockDispatcher("gaia")
+    return mock_dispatcher
+
+
+@pytest.fixture(scope="function")
+def events_handler(mock_dispatcher: MockDispatcher, engine: Engine):
+    events_handler = Events(engine)
+    mock_dispatcher.register_event_handler(events_handler)
+    engine.message_broker = mock_dispatcher
+    engine.event_handler = events_handler
+
+    try:
+        yield events_handler
+    finally:
+        mock_dispatcher.clear_store()
