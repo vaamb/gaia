0c37f6b3d3752848de9e17be030414129c845113
Valentin Ambroise <113367796+vaamb@users.noreply.github.com>
2024-06-14 18:47:42 +0200

    Move `gpioDimmer` from `hardware.abc` to `hardware.actuators.GPIO` (#147)

--- src/gaia/hardware/abc.py
+++ src/gaia/hardware/abc.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 from datetime import datetime, timezone
-import enum
 from enum import Enum
 import io
 import os
@@ -27,10 +26,9 @@ if t.TYPE_CHECKING:  # pragma: no cover
     from gaia.subroutines.template import SubroutineTemplate
 
     if is_raspi():
-        import pwmio
         from adafruit_blinka.microcontroller.bcm283x.pin import Pin
     else:
-        from gaia.hardware._compatibility import Pin, pwmio
+        from gaia.hardware._compatibility import Pin
 
 
 class Measure(Enum):
@@ -478,47 +476,6 @@ class Dimmer(Hardware):
         raise NotImplementedError(
             "This method must be implemented in a subclass"
         )  # pragma: no cover
-
-
-class gpioDimmer(gpioHardware, Dimmer):
-    def __init__(self, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
-        if not self._address_book.secondary.type == AddressType.GPIO:  # pragma: no cover
-            raise ValueError(
-                "gpioDimmable address must be of type"
-                "'addressType1_addressNum1:GPIO_pinNumber'"
-            )
-        self._pwm_pin: "Pin" | None = None
-        self._dimmer : "pwmio.PWMOut" | None = None
-
-    def _get_dimmer(self) -> "pwmio.PWMOut":
-        if is_raspi():
-            try:
-                import pwmio
-            except ImportError:
-                raise RuntimeError(
-                    "Adafruit blinka package is required. Run `pip install "
-                    "adafruit-blinka` in your virtual env`."
-                )
-        else:
-            from gaia.hardware._compatibility import pwmio
-        return pwmio.PWMOut(self.pwm_pin, frequency=100, duty_cycle=0)
-
-    def set_pwm_level(self, duty_cycle_in_percent: float | int) -> None:
-        duty_cycle_in_16_bit = duty_cycle_in_percent / 100 * (2**16 - 1)
-        self.dimmer.duty_cycle = duty_cycle_in_16_bit
-
-    @property
-    def pwm_pin(self) -> "Pin":
-        if not self._pwm_pin:
-            self._pwm_pin = self._get_pin(self._address_book.secondary.main)
-        return self._pwm_pin
-
-    @property
-    def dimmer(self) -> "pwmio.PWMOut":
-        if not self._dimmer:
-            self._dimmer = self._get_dimmer()
-        return self._dimmer
 
 
 class i2cHardware(Hardware):
