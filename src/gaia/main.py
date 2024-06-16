3f74acda3040fd0f0299d225a63972dee78ae3c3
Valentin Ambroise <valentin.ambroise@outlook.com>
2024-06-16 19:48:35 +0200

    Make `main` async

--- src/gaia/main.py
+++ src/gaia/main.py
@@ -1,18 +1,11 @@
+import asyncio
+
 import click
 
 
-@click.command()
-@click.option(
-    "--use-green-threads", "-gt",
-    type=bool,
-    is_flag=True,
-    default=False,
-    help="Monkey patch Gaia with gevent to use green threads",
-    show_default=True,
-)
-def main(
+async def _main(
         use_green_threads: bool,
-) -> None:
+):
     """Launch Gaia
     """
     if use_green_threads:
@@ -28,8 +21,23 @@ def main(
 
     gaia_engine = Engine()
     if gaia_engine.plugins_needed:
-        gaia_engine.init_plugins()
-    gaia_engine.run()
+        await gaia_engine.init_plugins()
+    await gaia_engine.run()
+
+
+@click.command()
+@click.option(
+    "--use-green-threads", "-gt",
+    type=bool,
+    is_flag=True,
+    default=False,
+    help="Monkey patch Gaia with gevent to use green threads",
+    show_default=True,
+)
+def main(
+        use_green_threads: bool,
+) -> None:
+    asyncio.run(_main(use_green_threads))
 
 
 if __name__ == "__main__":
