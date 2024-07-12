4cd2e056b2f5c7ad3bfe379c1b65a7847d454707
Valentin Ambroise <valentin.ambroise@outlook.com>
2024-07-12 23:52:06 +0200

    Don't send actuator state if not connected to Gaia

--- src/gaia/actuator_handler.py
+++ src/gaia/actuator_handler.py
@@ -557,7 +557,9 @@ class ActuatorHandler:
             uid=self.ecosystem.uid,
             data=[data],
         ).model_dump()
-        sent = await self.ecosystem.event_handler.emit("actuators_data", data=[payload])
+        sent = False
+        if self.ecosystem.event_handler.is_connected():
+            sent = await self.ecosystem.event_handler.emit("actuators_data", data=[payload])
         # If not sent, and the db is enabled, save the change in the db buffer
         if sent or not self.ecosystem.engine.use_db:
             return
