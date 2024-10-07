ebe9b997c853439046add961b175988e5cae8a01
Valentin Ambroise <valentin.ambroise@outlook.com>
2024-10-08 01:14:40 +0200

    Try to pass the GitHub actions

--- tests/conftest.py
+++ tests/conftest.py
@@ -32,16 +32,16 @@ T = TypeVar("T")
 YieldFixture = Generator[T, None, None]
 
 
-@pytest.fixture(scope="session")
-def event_loop():
-    if sys.platform.startswith("win") and sys.version_info[:2] >= (3, 8):
-        # Avoid "RuntimeError: Event loop is closed" on Windows when tearing down tests
-        # https://github.com/encode/httpx/issues/914
-        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-
-    loop = asyncio.new_event_loop()
-    yield loop
-    loop.close()
+#@pytest.fixture(scope="session")
+#def event_loop():
+#    if sys.platform.startswith("win") and sys.version_info[:2] >= (3, 8):
+#        # Avoid "RuntimeError: Event loop is closed" on Windows when tearing down tests
+#        # https://github.com/encode/httpx/issues/914
+#        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+#
+#    loop = asyncio.new_event_loop()
+#    yield loop
+#    loop.close()
 
 
 @pytest.fixture(scope="session")
