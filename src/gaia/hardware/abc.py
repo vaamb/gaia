84bd6ce67356fe76db3e6302b80fad066841f6a5
Valentin Ambroise <113367796+vaamb@users.noreply.github.com>
2024-10-08 00:53:54 +0200

    Fix some failing tests (#187)

--- src/gaia/hardware/abc.py
+++ src/gaia/hardware/abc.py
@@ -251,7 +251,8 @@ class Hardware(metaclass=_MetaHardware):
         if subroutine is None:
             self._subroutine = None
         else:
-            self._subroutine = weakref.proxy(subroutine)
+            # TODO: use weakref again ?
+            self._subroutine = subroutine  # weakref.proxy(subroutine)
         self._uid: str = uid
         self._name: str = name
         self._level: gv.HardwareLevel = level
