cea7b172a05492df7503e3cb8a331393e1d43131
Valentin Ambroise <valentin.ambroise@outlook.com>
2024-10-10 00:12:15 +0200

    Do not cover uncommon error branches in `dependencies.*.py

--- src/gaia/dependencies/camera.py
+++ src/gaia/dependencies/camera.py
@@ -4,7 +4,7 @@ _uninstalled_dependencies = False
 
 try:
     import numpy as np
-except ImportError:
+except ImportError:  # pragma: no cover
     np = None
     _uninstalled_dependencies = True
 
@@ -12,7 +12,7 @@ try:
     import PIL
     from PIL import Image as PIL_image
 
-except ImportError:
+except ImportError:  # pragma: no cover
     class _Image:
         Image = None
 
@@ -22,14 +22,14 @@ except ImportError:
 
 try:
     import skimage
-except ImportError:
+except ImportError:  # pragma: no cover
     skimage = None
 #    _uninstalled_dependencies = True
 
 
 try:
     from gaia_validators.image import SerializableImage, SerializableImagePayload
-except ImportError:
+except ImportError:  # pragma: no cover
     SerializableImage = None
     SerializableImagePayload = None
     _uninstalled_dependencies = True
@@ -46,14 +46,14 @@ if t.TYPE_CHECKING:  # pragma: no cover
 
 def check_dependencies(check_skimage: bool = True) -> None:
     if check_skimage:
-        if _uninstalled_dependencies is True or skimage is None:
+        if _uninstalled_dependencies is True or skimage is None:  # pragma: no cover
             raise RuntimeError(
                 "All the dependencies required to use the camera have not been "
                 "installed. Run 'pip install . [camera]' in your virtual "
                 "environment to install them."
             )
     else:
-        if _uninstalled_dependencies is True:
+        if _uninstalled_dependencies is True:  # pragma: no cover
             raise RuntimeError(
                 "All the dependencies required to use the camera have not been "
                 "installed. Run 'pip install . [camera]' in your virtual "
