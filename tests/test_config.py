1d69472f09be26608e1956e3a8b602e384fac661
Valentin Ambroise <113367796+vaamb@users.noreply.github.com>
2024-10-10 22:27:52 +0200

    Test all `Hardware` models available (#197)
    * Move the tests from `test_hardware.py` to `test_config.py` as they all test the generation of hardware config
    
    * Test all the hardware models available and make sure the required functions are implemented

--- tests/test_config.py
+++ tests/test_config.py
@@ -6,11 +6,12 @@ import pytest
 import gaia_validators as gv
 
 from gaia.config import ConfigType, EcosystemConfig, EngineConfig
-from gaia.exceptions import UndefinedParameter
+from gaia.exceptions import HardwareNotFound, UndefinedParameter
 from gaia.subroutines import subroutine_names
 from gaia.utils import yaml
 
-from .data import ecosystem_info, ecosystem_name, sun_times
+from .data import (
+    ecosystem_info, ecosystem_name, sensor_info, sensor_uid, sun_times)
 from .utils import get_logs_content
 
 
@@ -255,3 +256,92 @@ async def test_ecosystem_time_parameters(ecosystem_config: EcosystemConfig):
 
     await ecosystem_config.set_nycthemeral_span_hours({"day": "4h21", "night": "22h00"})
     assert ecosystem_config.nycthemeral_span_hours.day == time(4, 21)
+
+
+# Test hardware config
+def test_hardware_creation_fail_address(ecosystem_config: EcosystemConfig):
+    with pytest.raises(ValueError, match=r"Address .* already used"):
+        ecosystem_config.create_new_hardware(**sensor_info)
+
+
+def test_hardware_creation_fail_model(ecosystem_config: EcosystemConfig):
+    invalid_hardware_info = {
+        **sensor_info,
+        "address": "GPIO_11",  # Use a free address
+        "model": "Invalid"
+    }
+    with pytest.raises(ValueError, match="This hardware model is not supported"):
+        ecosystem_config.create_new_hardware(**invalid_hardware_info)
+
+
+def test_hardware_creation_fail_type(ecosystem_config: EcosystemConfig):
+    invalid_hardware_info = {
+        **sensor_info,
+        "address": "GPIO_7",  # Use a free address
+        "type": "Invalid",
+    }
+    error_msg = "VALUE ERROR at parameter 'type', input 'Invalid' is not valid"
+    with pytest.raises(ValueError, match=error_msg):
+        ecosystem_config.create_new_hardware(**invalid_hardware_info)
+
+
+def test_hardware_creation_fail_level(ecosystem_config: EcosystemConfig):
+    invalid_hardware_info = {
+        **sensor_info,
+        "address": "GPIO_7",  # Use a free address
+        "level": "Invalid",
+    }
+    error_msg = "VALUE ERROR at parameter 'level', input 'Invalid' is not valid"
+    with pytest.raises(ValueError, match=error_msg):
+        ecosystem_config.create_new_hardware(**invalid_hardware_info)
+
+
+def test_hardware_creation_success(ecosystem_config: EcosystemConfig):
+    valid_hardware_info = {
+        **sensor_info,
+        "model": "gpioSwitch",
+        "address": "GPIO_11",  # Use a free address
+    }
+    ecosystem_config.create_new_hardware(**valid_hardware_info)
+
+
+def test_hardware_update_fail_not_found(ecosystem_config: EcosystemConfig):
+    with pytest.raises(HardwareNotFound):
+        ecosystem_config.update_hardware("invalid_uid", address="GPIO_7")
+
+
+def test_hardware_update_fail_address(ecosystem_config: EcosystemConfig):
+    with pytest.raises(ValueError, match=r"Address .* already used"):
+        hardware_address = sensor_info["address"]
+        ecosystem_config.update_hardware(sensor_uid, address=hardware_address)
+
+
+def test_hardware_update_fail_model(ecosystem_config: EcosystemConfig):
+    with pytest.raises(ValueError, match="This hardware model is not supported"):
+        ecosystem_config.update_hardware(sensor_uid, model="Invalid")
+
+
+def test_hardware_update_fail_type(ecosystem_config: EcosystemConfig):
+    error_msg = "VALUE ERROR at parameter 'type', input 'Invalid' is not valid"
+    with pytest.raises(ValueError, match=error_msg):
+        ecosystem_config.update_hardware(sensor_uid, type="Invalid")
+
+
+def test_hardware_update_fail_level(ecosystem_config: EcosystemConfig):
+    error_msg = "VALUE ERROR at parameter 'level', input 'Invalid' is not valid"
+    with pytest.raises(ValueError, match=error_msg):
+        ecosystem_config.update_hardware(sensor_uid, level="Invalid")
+
+
+def test_hardware_update_success(ecosystem_config: EcosystemConfig):
+    ecosystem_config.update_hardware(
+        sensor_uid, model="gpioSwitch", address="GPIO_11")
+
+
+def test_hardware_delete_fail_not_found(ecosystem_config: EcosystemConfig):
+    with pytest.raises(HardwareNotFound):
+        ecosystem_config.delete_hardware("invalid_uid")
+
+
+def test_hardware_delete_success(ecosystem_config: EcosystemConfig):
+    ecosystem_config.delete_hardware(sensor_uid)
--- tests/test_hardware.py
+++ tests/test_hardware.py
@@ -1,98 +1,92 @@
+from typing import Type
+
 import pytest
 
-from gaia.config import EcosystemConfig
-from gaia.exceptions import HardwareNotFound
+import gaia_validators as gv
 
-from .data import sensor_info, sensor_uid
-
-
-hardware_info = sensor_info
-hardware_uid = sensor_uid
-
-
-def test_hardware_creation_fail_address(ecosystem_config: EcosystemConfig):
-    with pytest.raises(ValueError, match=r"Address .* already used"):
-        ecosystem_config.create_new_hardware(**hardware_info)
+from gaia.hardware import hardware_models
+from gaia.hardware.abc import (
+    _MetaHardware, BaseSensor, Camera, Dimmer, gpioHardware, Hardware,
+    i2cHardware, PlantLevelHardware, Switch)
+from gaia.hardware.camera import PiCamera
+from gaia.utils import create_uid
 
 
-def test_hardware_creation_fail_model(ecosystem_config: EcosystemConfig):
-    invalid_hardware_info = {
-        **hardware_info,
-        "address": "GPIO_11",  # Use a free address
-        "model": "Invalid"
-    }
-    with pytest.raises(ValueError, match="This hardware model is not supported"):
-        ecosystem_config.create_new_hardware(**invalid_hardware_info)
+@pytest.mark.asyncio
+async def test_hardware_models():
+    for hardware_cls in hardware_models.values():
+        # Create required config
+        try:
+            hardware_cls: Type[Hardware]
+            hardware_cfg: gv.HardwareConfigDict = {
+                "uid": create_uid(16),
+                "name": "VirtualTestHardware",
+                "address": None,
+                "model": None,
+                "type": None,
+                "level": None,
+                "measures": [],
+                "plants": [],
+                "multiplexer_model": None,
+            }
+            # Setup address
+            if issubclass(hardware_cls, gpioHardware):
+                hardware_cfg["address"] = "GPIO_19&GPIO_12"
+            elif issubclass(hardware_cls, i2cHardware):
+                hardware_cfg["address"] = "I2C_default"
+            elif issubclass(hardware_cls, PiCamera):
+                hardware_cfg["address"] = "picamera"
+            else:
+                raise ValueError("Unknown hardware address")
+            # Setup model
+            hardware_cfg["model"] = hardware_cls.__name__
+            # Setup type
+            if issubclass(hardware_cls, BaseSensor):
+                hardware_cfg["type"] = gv.HardwareType.sensor
+            elif issubclass(hardware_cls, Camera):
+                hardware_cfg["type"] = gv.HardwareType.camera
+            elif issubclass(hardware_cls, (Dimmer, Switch)):
+                hardware_cfg["type"] = gv.HardwareType.light
+            else:
+                raise ValueError("Unknown hardware type")
+            # Setup measures
+            if issubclass(hardware_cls, BaseSensor):
+                hardware_cfg["measures"] = [
+                    measure.name for measure in
+                    hardware_cls.measures_available.keys()
+                ]
+            # Setup plants
+            if issubclass(hardware_cls, PlantLevelHardware):
+                hardware_cfg["level"] = gv.HardwareLevel.plants
+                hardware_cfg["plants"] = ["VirtualTestPlant"]
+            else:
+                hardware_cfg["level"] = gv.HardwareLevel.environment
+        except Exception as e:
+            raise Exception(
+                f"Error while setting up config for {hardware_cls}."
+            ) from e
 
+        # Test hardware
+        try:
+            hardware = hardware_cls.from_unclean(subroutine=None, **hardware_cfg)
+            if isinstance(hardware, gpioHardware):
+                assert hardware.pin
+            if isinstance(hardware, i2cHardware):
+                hardware._get_i2c()
+            if isinstance(hardware, PlantLevelHardware):
+                assert hardware.plants
+            if isinstance(hardware, BaseSensor):
+                assert await hardware.get_data()
+            if isinstance(hardware, Camera):
+                assert await hardware.get_image((42, 21))
+            if isinstance(hardware, Dimmer):
+                await hardware.set_pwm_level(100)
+            if isinstance(hardware, Switch):
+                await hardware.turn_on()
+                await hardware.turn_off()
+            print(f"Test succeeded for hardware '{hardware}'")
+        except Exception as e:
+            raise Exception(f"Error while testing {hardware_cls}.") from e
 
-def test_hardware_creation_fail_type(ecosystem_config: EcosystemConfig):
-    invalid_hardware_info = {
-        **hardware_info,
-        "address": "GPIO_7",  # Use a free address
-        "type": "Invalid",
-    }
-    error_msg = "VALUE ERROR at parameter 'type', input 'Invalid' is not valid"
-    with pytest.raises(ValueError, match=error_msg):
-        ecosystem_config.create_new_hardware(**invalid_hardware_info)
-
-
-def test_hardware_creation_fail_level(ecosystem_config: EcosystemConfig):
-    invalid_hardware_info = {
-        **hardware_info,
-        "address": "GPIO_7",  # Use a free address
-        "level": "Invalid",
-    }
-    error_msg = "VALUE ERROR at parameter 'level', input 'Invalid' is not valid"
-    with pytest.raises(ValueError, match=error_msg):
-        ecosystem_config.create_new_hardware(**invalid_hardware_info)
-
-
-def test_hardware_creation_success(ecosystem_config: EcosystemConfig):
-    valid_hardware_info = {
-        **hardware_info,
-        "model": "gpioSwitch",
-        "address": "GPIO_11",  # Use a free address
-    }
-    ecosystem_config.create_new_hardware(**valid_hardware_info)
-
-
-def test_hardware_update_fail_not_found(ecosystem_config: EcosystemConfig):
-    with pytest.raises(HardwareNotFound):
-        ecosystem_config.update_hardware("invalid_uid", address="GPIO_7")
-
-
-def test_hardware_update_fail_address(ecosystem_config: EcosystemConfig):
-    with pytest.raises(ValueError, match=r"Address .* already used"):
-        hardware_address = sensor_info["address"]
-        ecosystem_config.update_hardware(hardware_uid, address=hardware_address)
-
-
-def test_hardware_update_fail_model(ecosystem_config: EcosystemConfig):
-    with pytest.raises(ValueError, match="This hardware model is not supported"):
-        ecosystem_config.update_hardware(hardware_uid, model="Invalid")
-
-
-def test_hardware_update_fail_type(ecosystem_config: EcosystemConfig):
-    error_msg = "VALUE ERROR at parameter 'type', input 'Invalid' is not valid"
-    with pytest.raises(ValueError, match=error_msg):
-        ecosystem_config.update_hardware(hardware_uid, type="Invalid")
-
-
-def test_hardware_update_fail_level(ecosystem_config: EcosystemConfig):
-    error_msg = "VALUE ERROR at parameter 'level', input 'Invalid' is not valid"
-    with pytest.raises(ValueError, match=error_msg):
-        ecosystem_config.update_hardware(hardware_uid, level="Invalid")
-
-
-def test_hardware_update_success(ecosystem_config: EcosystemConfig):
-    ecosystem_config.update_hardware(
-        hardware_uid, model="gpioSwitch", address="GPIO_11")
-
-
-def test_hardware_delete_fail_not_found(ecosystem_config: EcosystemConfig):
-    with pytest.raises(HardwareNotFound):
-        ecosystem_config.delete_hardware("invalid_uid")
-
-
-def test_hardware_delete_success(ecosystem_config: EcosystemConfig):
-    ecosystem_config.delete_hardware(hardware_uid)
+        finally:
+            _MetaHardware.instances.clear()
