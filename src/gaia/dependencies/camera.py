3dca64ce73e0fe16b8e595de5a51ccd17611b284
Valentin Ambroise <113367796+vaamb@users.noreply.github.com>
2024-11-07 21:37:04 +0100

    Use `gaia_validators`'new `SerializableImage` as the base class for image manipulation (#208)
    * Use `gaia_validators`'`SerializableImage` as the base class for image manipulation
    
    - Remove the need of `pillow`
    
    * Remove `pillow` from dependencies

--- src/gaia/dependencies/camera.py
+++ src/gaia/dependencies/camera.py
@@ -9,23 +9,10 @@ except ImportError:  # pragma: no cover
     _uninstalled_dependencies = True
 
 try:
-    import PIL
-    from PIL import Image as PIL_image
-
-except ImportError:  # pragma: no cover
-
-    class _Image:
-        Image = None
-
-    PIL = None
-    PIL_image = _Image()
-    _uninstalled_dependencies = True
-
-try:
     import cv2
 except ImportError:  # pragma: no cover
     cv2 = None
-#    _uninstalled_dependencies = True
+    _uninstalled_dependencies = True
 
 
 try:
@@ -38,25 +25,15 @@ except ImportError:  # pragma: no cover
 
 if t.TYPE_CHECKING:  # pragma: no cover
     import numpy as np
-    import PIL
-    from PIL import Image as PIL_image
     import cv2
 
     from gaia_validators.image import SerializableImage, SerializableImagePayload
 
 
 def check_dependencies(check_cv2: bool = True) -> None:
-    if check_cv2:
-        if _uninstalled_dependencies is True or cv2 is None:  # pragma: no cover
-            raise RuntimeError(
-                "All the dependencies required to use the camera have not been "
-                "installed. Run 'pip install . [camera]' in your virtual "
-                "environment to install them."
-            )
-    else:
-        if _uninstalled_dependencies is True:  # pragma: no cover
-            raise RuntimeError(
-                "All the dependencies required to use the camera have not been "
-                "installed. Run 'pip install . [camera]' in your virtual "
-                "environment to install them."
-            )
+    if _uninstalled_dependencies is True:  # pragma: no cover
+        raise RuntimeError(
+            "All the dependencies required to use the camera have not been "
+            "installed. Run 'pip install . [camera]' in your virtual "
+            "environment to install them."
+        )
