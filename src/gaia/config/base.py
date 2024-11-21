34752aaeef7ac877007bc05a93a0a21b75516566
Valentin Ambroise <113367796+vaamb@users.noreply.github.com>
2024-11-21 20:04:31 +0100

    Allow to send compressed picture to Ouranos (#217)

--- src/gaia/config/base.py
+++ src/gaia/config/base.py
@@ -45,6 +45,7 @@ class BaseConfig:
     PICTURE_TAKING_PERIOD = 20.0  # in seconds
     PICTURE_SENDING_PERIOD = 120.0  # in seconds, should be a multiple of previous
     PICTURE_SIZE = (1640, 1232)  # in pixel
+    PICTURE_COMPRESSION_FORMAT = ".jpeg"  # None, .jpeg, .png
     PICTURE_TRANSFER_METHOD = os.environ.get("PICTURE_TRANSFER_METHOD", "broker")  # broker or upload
     SENSORS_LOOP_PERIOD = 10.0  # in s
     SENSORS_LOGGING_PERIOD = "*/10"  # in minute, cron-style
