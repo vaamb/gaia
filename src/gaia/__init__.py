8f159ff4b73513b72107d58e45bed4ea6a97f0e4
Valentin Ambroise <113367796+vaamb@users.noreply.github.com>
2025-08-14 23:31:04 +0200

    Simplify the import mechanism in main's `__init__.py` as circular imports have been removed (#262)

--- src/gaia/__init__.py
+++ src/gaia/__init__.py
@@ -1,24 +1,7 @@
 __version__ = "0.9.0"
 
-import typing as t
-
-
-if t.TYPE_CHECKING:  # pragma: no cover
-    from gaia.config import (
-        BaseConfig, EcosystemConfig, EngineConfig, GaiaConfigHelper)
-    from gaia.ecosystem import Ecosystem
-    from gaia.engine import Engine
-    from gaia.main import main
-else:
-    from importlib import import_module
-
-    from gaia.main import main
-
-    def __getattr__(name):
-        if name in (
-                "BaseConfig", "EcosystemConfig", "EngineConfig", "GaiaConfigHelper"):
-            return getattr(import_module("gaia.config"), name)
-        if name == "Ecosystem":
-            return getattr(import_module("gaia.ecosystem"), name)
-        if name == "Engine":
-            return getattr(import_module("gaia.engine"), name)
+from gaia.config import (
+    BaseConfig, EcosystemConfig, EngineConfig, GaiaConfigHelper)
+from gaia.ecosystem import Ecosystem
+from gaia.engine import Engine
+from gaia.main import main
