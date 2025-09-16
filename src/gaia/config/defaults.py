7b056c140f5102b7bb4923d42f6b64fc1c239b83
Valentin Ambroise <valentin.ambroise@outlook.com>
2025-09-16 03:23:22 +0200

    Add methods to get ecosystem-specific `actuator_couples` and its related dicts

--- src/gaia/config/defaults.py
+++ src/gaia/config/defaults.py
@@ -21,17 +21,27 @@ assert all([
 ])
 
 
-actuator_to_parameter: dict[str, gv.ClimateParameter] = {
-    actuator: climate_parameter
-    for climate_parameter, actuator_couple in actuator_couples.items()
-    for actuator in actuator_couple
-    if actuator is not None
-}
+def get_actuator_to_parameter(
+        actuator_couples: dict[gv.ClimateParameter, gv.ActuatorCouple],
+) -> dict[str, gv.ClimateParameter]:
+    return {
+        actuator: climate_parameter
+        for climate_parameter, actuator_couple in actuator_couples.items()
+        for actuator in actuator_couple
+        if actuator is not None
+    }
+
+actuator_to_parameter = get_actuator_to_parameter(actuator_couples)
 
 
-actuator_to_direction: dict[str, Literal["increase", "decrease"]] = {
-    actuator: direction
-    for climate_parameter, actuator_couple in actuator_couples.items()
-    for actuator, direction in zip(actuator_couple, ("increase", "decrease"))
-    if actuator is not None
-}
+def get_actuator_to_direction(
+        actuator_couples: dict[gv.ClimateParameter, gv.ActuatorCouple],
+) -> dict[str, Literal["increase", "decrease"]]:
+    return {
+        actuator: direction
+        for climate_parameter, actuator_couple in actuator_couples.items()
+        for actuator, direction in zip((actuator_couple), ("increase", "decrease"))
+        if actuator is not None
+    }
+
+actuator_to_direction = get_actuator_to_direction(actuator_couples)
