85e694869feda7a1598b8451663812c01d8e0ae0
Valentin Ambroise <valentin.ambroise@outlook.com>
2025-09-23 20:59:49 +0200

    Add tests
    - Also improve `test_on_registration_ack()`

--- tests/data.py
+++ tests/data.py
@@ -188,6 +188,19 @@ climate_dict: dict[str, gv.AnonymousClimateConfigDict] = {
 }
 
 
+rain_cfg: gv.AnonymousWeatherConfigDict = {
+    "pattern": "0 * * * *",
+    "duration": 30,
+    "level": 100.0,
+    "linked_actuator": "rainer",
+}
+
+
+weather_cfg: dict[str, gv.AnonymousWeatherConfigDict] = {
+    "rain": rain_cfg,
+}
+
+
 ecosystem_info = {
     ecosystem_uid: {
         "name": ecosystem_name,
@@ -214,6 +227,7 @@ ecosystem_info = {
                 "lighting": lighting_method,
             },
             "climate": climate_dict,
+            "weather": weather_cfg,
         },
         "IO": IO_dict,
         "plants": {},
