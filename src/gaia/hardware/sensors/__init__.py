e6813e092759d5faa0ba2ada096460df449a7fa7
Valentin Ambroise <valentin.ambroise@outlook.com>
2026-01-07 21:55:07 +0100

    Add `BS18B20` hardware

--- src/gaia/hardware/sensors/__init__.py
+++ src/gaia/hardware/sensors/__init__.py
@@ -1,14 +1,15 @@
 from typing import Type
 
 from gaia.hardware.abc import BaseSensor
-from gaia.hardware.sensors.abc import TempHumSensor
 from gaia.hardware.sensors.GPIO import gpio_sensor_models
 from gaia.hardware.sensors.I2C import i2c_sensor_models
 from gaia.hardware.sensors.virtual import virtual_sensor_models
+from gaia.hardware.sensors.onewire import onewire_sensor_models
 
 
 sensor_models: dict[str, Type[BaseSensor]] = {
     **gpio_sensor_models,
     **i2c_sensor_models,
     **virtual_sensor_models,
+    **onewire_sensor_models,
 }
---
+++ src/gaia/hardware/sensors/onewire.py
@@ -0,0 +1,30 @@
+import typing as t
+from typing import Type
+
+from gaia.hardware.abc import BaseSensor, OneWireHardware
+from gaia.hardware.sensors.abc import TemperatureSensor
+from gaia.hardware.utils import is_raspi
+
+if t.TYPE_CHECKING:
+    if is_raspi():
+        from gaia.hardware.sensors._devices.gaia_bs18b20 import BS18B20
+
+
+class BS18B20(OneWireHardware, TemperatureSensor):
+    def _get_device(self) -> "BS18B20":
+        if is_raspi():
+            from gaia.hardware.sensors._devices.gaia_bs18b20 import BS18B20 as _BS18B20
+        else:
+            from gaia.hardware._compatibility import BS18B20 as _BS18B20
+        return _BS18B20()
+
+    def _get_raw_data(self) -> tuple[float | None]:
+        return self.device.get_data()
+
+
+onewire_sensor_models: dict[str, Type[BaseSensor]] = {
+    hardware.__name__: hardware
+    for hardware in [
+        BS18B20,
+    ]
+}
