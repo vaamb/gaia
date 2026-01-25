58341e208c7a39bebd1e895c4869ddbbf36a97cb
Valentin Ambroise <valentin.ambroise@outlook.com>
2026-01-25 19:19:56 +0100

    Make ruff happy

--- src/gaia/hardware/_websocket.py
+++ src/gaia/hardware/_websocket.py
@@ -88,7 +88,7 @@ class WebSocketHardwareManager:
             self.device_connections.pop(device_uid)
 
     def get_connection(self, device_uid: str) -> ServerConnection | None:
-        if not device_uid in self._registered_hardware:
+        if device_uid not in self._registered_hardware:
             raise RuntimeError(f"Hardware {device_uid} was never registered")
         try:
             return self.device_connections[device_uid]
