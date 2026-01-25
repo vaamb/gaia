4b7676be10a17674003ee0a3e6eb34ab0d3d78f0
Valentin Ambroise <valentin.ambroise@outlook.com>
2026-01-25 21:31:56 +0100

    `WebSocket{Sensor,Switch,Dimmer}` aren't ABCs

---
+++ src/gaia/hardware/actuators/websocket.py
@@ -0,0 +1,58 @@
+from typing import Type
+
+import gaia_validators as gv
+
+from gaia.hardware.abc import Dimmer, Hardware, Switch, WebSocketHardware
+
+
+class WebSocketSwitch(Switch, WebSocketHardware):
+    async def turn_on(self) -> None:
+        try:
+            response = await self._send_msg_and_wait({"action": "turn_actuator", "data": "on"})
+            response = gv.RequestResult.model_validate(response)
+        except ConnectionError:
+            self._logger.error("Could not connect to the device")
+        else:
+            if response.status != gv.Result.success:
+                base_msg = "Failed to turn on the switch"
+                if response.message:
+                    base_msg = f"{base_msg}. Error msg: `{response.message}`."
+                self._logger.error(base_msg)
+
+
+    async def turn_off(self) -> None:
+        try:
+            response = await self._send_msg_and_wait({"action": "turn_actuator", "data": "off"})
+            response = gv.RequestResult.model_validate(response)
+        except ConnectionError:
+            self._logger.error("Could not connect to the device")
+        else:
+            if response.status != gv.Result.success:
+                base_msg = "Failed to turn off the switch"
+                if response.message:
+                    base_msg = f"{base_msg}. Error msg: `{response.message}`."
+                self._logger.error(base_msg)
+
+
+class WebSocketDimmer(Dimmer, WebSocketHardware):
+    async def set_pwm_level(self, level) -> None:
+        try:
+            response = await self._send_msg_and_wait({"action": "set_level", "data": level})
+            response = gv.RequestResult.model_validate(response)
+        except ConnectionError:
+            self._logger.error("Could not connect to the device")
+        else:
+            if response.status != gv.Result.success:
+                base_msg = f"Failed to set the level to {level}"
+                if response.message:
+                    base_msg = f"{base_msg}. Error msg: `{response.message}`."
+                self._logger.error(base_msg)
+
+
+websocket_actuator_models: dict[str, Type[Hardware]] = {
+    hardware.__name__: hardware
+    for hardware in [
+        WebSocketDimmer,
+        WebSocketSwitch,
+    ]
+}
---
+++ src/gaia/hardware/sensors/websocket.py
@@ -0,0 +1,33 @@
+from typing import Type
+
+from pydantic import RootModel, ValidationError
+
+import gaia_validators as gv
+
+from gaia.hardware.abc import BaseSensor, WebSocketHardware
+
+
+SensorRecords = RootModel[list[gv.SensorRecord]]
+
+
+class WebSocketSensor(BaseSensor, WebSocketHardware):
+    async def get_data(self) -> list[gv.SensorRecord]:
+        try:
+            data = await self._send_msg_and_wait({"action": "send_data"})
+        except ConnectionError:
+            return []
+        try:
+            data: list[gv.SensorRecord] = SensorRecords.model_validate(data).model_dump()
+        except ValidationError:
+            self._logger.error(f"Received an invalid `SensorRecord`: {data}")
+            return []
+        else:
+            return data
+
+
+websocket_sensor_models: dict[str, Type[BaseSensor]] = {
+    hardware.__name__: hardware
+    for hardware in [
+        WebSocketSensor,
+    ]
+}
