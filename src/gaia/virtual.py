vaamb/gaia

commit db87a12a64a9dfee255d4156455de97919e9b73a

Add `virtualActuator`s that can interact with `VirtualEcosystem` (#419)
* Make `VirtualHardware` hold a reference to its related `VirtualEcosystem`

* Add `virtualActuator`s and make them interact with `VirtualEcosystem`

* Fix ty's issues

src/gaia/virtual.py
@@ -1,11 +1,12 @@
 from __future__ import annotations
 
+from dataclasses import dataclass, field
 from datetime import datetime, time, timedelta
 import logging
 import math
 from time import monotonic
 import typing
-from typing import Self
+from typing import Literal, Self, TypeAlias
 from weakref import WeakValueDictionary
 
 import gaia_validators as gv
@@ -21,6 +22,16 @@ if typing.TYPE_CHECKING:
 
 SUNRISE = time(7, 0)
 SUNSET = time(19, 0)
+
+Direction = Literal["increase", "decrease"]
+ClimateDirection: TypeAlias = tuple[gv.ClimateParameter, Direction]
+HardwareUid: TypeAlias = str
+
+@dataclass
+class ActuatorState:
+    status: bool = False
+    level: float = 100.0
+    groups: set[str] = field(default_factory=set)
 
 
 # 1 watt = 1 joule / sec
@@ -213,6 +224,9 @@ class VirtualEcosystem:
         self._start_time: float | None = None
         self._last_update: float | None = None
 
+        self._get_actuator_mapping = self.ecosystem.config.get_environment_direction_to_group
+        self._actuators: dict[HardwareUid, ActuatorState] = {}
+
         # Add the virtual ecosystem to the registry
         VirtualEcosystem._instances[ecosystem.uid] = self
 
@@ -285,15 +299,29 @@ class VirtualEcosystem:
     def status(self) -> bool:
         return self._start_time is not None
 
-    def get_actuator_status(self, actuator_group: str) -> bool:
-        if actuator_group in self.ecosystem.actuator_hub.actuator_handlers:
-            return self.ecosystem.actuator_hub.get_handler(actuator_group).status
-        return False
+    def register_actuator(self, actuator_uid: HardwareUid, groups: set[str]) -> None:
+        self._actuators[actuator_uid] = ActuatorState(groups=groups)
 
-    def get_actuator_level(self, actuator_group: str) -> float | None:
-        if actuator_group in self.ecosystem.actuator_hub.actuator_handlers:
-            return self.ecosystem.actuator_hub.get_handler(actuator_group).level
-        return None
+    def unregister_actuator(self, actuator_uid: HardwareUid) -> None:
+        self._actuators.pop(actuator_uid)
+
+    def set_actuator_status(self, actuator_uid: HardwareUid, status: bool) -> None:
+        self._actuators[actuator_uid].status = status
+
+    def get_actuator_status(self, actuator_uid: str) -> bool:
+        return self._actuators[actuator_uid].status
+
+    def set_actuator_level(self, actuator_uid: HardwareUid, level: int | float) -> None:
+        self._actuators[actuator_uid].level = level
+
+    def get_actuator_level(self, actuator_uid: HardwareUid) -> int | float:
+        return self._actuators[actuator_uid].level
+
+    def get_actuators_uid_for_group(self, actuator_group: str) -> list[HardwareUid]:
+        return [
+            actuator_uid for actuator_uid, actuator_state in self._actuators.items()
+            if actuator_group in actuator_state.groups
+        ]
 
     def measure(self, now: float | None = None) -> None:
         if not self._start_time:
@@ -320,60 +348,56 @@ class VirtualEcosystem:
             d_sec = now - self._last_update
         out_temp, out_hum, out_light = self.virtual_world.get_measures()
 
-        def get_corrected_level(actuator_group: str) -> float:
-            level = self.get_actuator_level(actuator_group)
+        def get_output(
+                actuator_group: str,
+                base_output: float,
+                correction_factor: float = 1.0,
+        ) -> float:
+            output = 0.0
+            for actuator_uid in self.get_actuators_uid_for_group(actuator_group):
+                if self.get_actuator_status(actuator_uid):
+                    level = get_corrected_level(actuator_uid)
+                    output += base_output * correction_factor * d_sec * level / 100
+            return output
+
+        def get_corrected_level(actuator_uid: HardwareUid) -> float:
+            level = self.get_actuator_level(actuator_uid)
             if level is None:
                 level = 100.0
             return level
 
-        actuators_mapping = self.ecosystem.config.get_environment_direction_to_group()
+        actuators_mapping = self._get_actuator_mapping()
 
-        # New heat quantity
+        # Heat quantity calculation
         d_temp = self.temperature - out_temp
         heat_quantity = self._heat_quantity
         heat_loss = self.heat_loss_coef * d_sec * d_temp
         heat_quantity -= heat_loss
-        temp_inc = actuators_mapping[(gv.ClimateParameter.temperature, "increase")]
-        if self.get_actuator_status(temp_inc):
-            level = get_corrected_level(temp_inc)
-            heater_output = self._max_heater_output * d_sec * level / 100
-            heat_quantity += heater_output
-        temp_dec = actuators_mapping[(gv.ClimateParameter.temperature, "decrease")]
-        if self.get_actuator_status(temp_dec):
-            level = get_corrected_level(temp_dec)
-            cooler_output = self._max_heater_output * 0.60 * d_sec * level / 100
-            heat_quantity -= cooler_output
-        self._heat_quantity = heat_quantity
+        temp_inc_group = actuators_mapping[(gv.ClimateParameter.temperature, "increase")]
+        heat_quantity += get_output(temp_inc_group, self._max_heater_output, 1.00)
+        temp_dec_group = actuators_mapping[(gv.ClimateParameter.temperature, "decrease")]
+        heat_quantity -= get_output(temp_dec_group, self._max_heater_output, 0.60)
 
         # Humidity calculation
         d_hum = self.absolute_humidity - get_absolute_humidity(out_temp, out_hum)
         humidity_quantity = self._humidity_quantity
         humidity_loss = d_hum * d_sec / 10000  # Pretty much a random factor
         humidity_quantity -= humidity_loss
-        hum_inc = actuators_mapping[(gv.ClimateParameter.humidity, "increase")]
-        if self.get_actuator_status(hum_inc):
-            level = get_corrected_level(hum_inc)
-            humidifier_output = self._max_humidifier_output * d_sec * level / 100
-            humidity_quantity += humidifier_output
-        hum_dec = actuators_mapping[(gv.ClimateParameter.humidity, "decrease")]
-        if self.get_actuator_status(hum_dec):
-            level = get_corrected_level(hum_dec)
-            dehumidifier_output = \
-                self._max_humidifier_output * 0.50 * d_sec * level / 100
-            humidity_quantity -= dehumidifier_output
-        self._humidity_quantity = humidity_quantity
+        hum_inc_group = actuators_mapping[(gv.ClimateParameter.humidity, "increase")]
+        humidity_quantity += get_output(hum_inc_group, self._max_humidifier_output, 1.00)
+        hum_dec_group = actuators_mapping[(gv.ClimateParameter.humidity, "decrease")]
+        humidity_quantity -= get_output(hum_dec_group, self._max_humidifier_output, 0.50)
 
         # Light calculation
         self._light = out_light
-        light_inc = actuators_mapping[(gv.ClimateParameter.light, "increase")]
-        if self.get_actuator_status(light_inc):
-            level = get_corrected_level(light_inc)
-            self._light += self._max_light_output * level / 100
+        light_inc_group = actuators_mapping[(gv.ClimateParameter.light, "increase")]
+        self._light += get_output(light_inc_group, self._max_light_output, 1.00)
+
+        # Update
         self._last_update = now
         self.logger.debug(
             f"Temperature: {self.temperature:2.2f} - humidity: {self.humidity:3.2f} - "
-            f"light: {self.light:.1f}"
-        )
+            f"light: {self.light:.1f}")
 
     def _compute_initial_hybrid_capacity(self) -> float:
         air_mass = self.volume * self.AIR_DENSITY

src/gaia/hardware/actuators/virtual.py
@@ -1,20 +1,80 @@
-from gaia.hardware.abc import Actuator
-from gaia.hardware.actuators.GPIO import gpioDimmer, gpioSwitch
+from gaia.hardware.abc import Actuator, Dimmer, Switch
+from gaia.hardware.actuators.GPIO import gpioDimmable, gpioDimmer, gpioSwitch
+from gaia.hardware.actuators.websocket import WebSocketDimmer, WebSocketSwitch
 from gaia.hardware.virtual import virtualHardware
 
 
-class virtualgpioSwitch(virtualHardware, gpioSwitch):
+class virtualActuator(virtualHardware, Actuator):
     __slots__ = ()
 
+    async def _on_initialize(self) -> None:
+        # Registration must be done before other registrations that might
+        #  interact with the virtual ecosystem
+        self.virtual_ecosystem.register_actuator(self.uid, self.groups)
+        await super()._on_initialize()
 
-class virtualgpioDimmable(virtualgpioSwitch, gpioDimmer):
+    async def _on_terminate(self) -> None:
+        await super()._on_terminate()
+        self.virtual_ecosystem.unregister_actuator(self.uid)
+
+
+class virtualSwitch(virtualActuator, Switch):
     __slots__ = ()
+
+    async def turn_on(self) -> bool:
+        self.virtual_ecosystem.set_actuator_status(self.uid, True)
+        return True
+
+    async def turn_off(self) -> bool:
+        self.virtual_ecosystem.set_actuator_status(self.uid, False)
+        return True
+
+    async def get_status(self) -> bool:
+        return self.virtual_ecosystem.get_actuator_status(self.uid)
+
+
+class virtualgpioSwitch(virtualSwitch, gpioSwitch):
+    pass
+
+
+class virtualWebSocketSwitch(virtualSwitch, WebSocketSwitch):
+    pass
+
+
+class virtualDimmer(virtualActuator, Dimmer):
+    __slots__ = ()
+
+    async def set_pwm_level(self, level: float | int) -> bool:
+        self.virtual_ecosystem.set_actuator_level(self.uid, level)
+        return True
+
+    async def get_pwm_level(self) -> int | float:
+        return self.virtual_ecosystem.get_actuator_level(self.uid)
+
+
+class virtualgpioDimmer(virtualDimmer, gpioDimmer):
+    pass
+
+
+class virtualWebSocketDimmer(virtualDimmer, WebSocketDimmer):
+    pass
+
+
+class virtualDimmable(virtualSwitch, virtualDimmer):
+    pass
+
+
+class virtualgpioDimmable(virtualDimmer, gpioDimmable):
+    pass
 
 
 virtual_actuator_models: dict[str, type[Actuator]] = {
     hardware.__name__: hardware
     for hardware in [
         virtualgpioDimmable,
+        virtualgpioDimmer,
         virtualgpioSwitch,
+        virtualWebSocketDimmer,
+        virtualWebSocketSwitch,
     ]
 }

src/gaia/hardware/sensors/virtual.py
@@ -8,7 +8,6 @@ from gaia.hardware.sensors.GPIO import DHTSensor
 from gaia.hardware.sensors.I2C import (
     AHT20, CapacitiveMoisture, ENS160, VCNL4040, VEML7700)
 from gaia.hardware.sensors.websocket import WebSocketSensor
-from gaia.hardware.utils import hardware_logger
 from gaia.hardware.virtual import virtualHardware
 
 
@@ -40,7 +39,7 @@ class virtualDHT11(virtualDHT):
     def _get_device(self) -> VirtualDHT11Device:
         from gaia.hardware.sensors._devices.virtual import VirtualDHT11Device
 
-        return VirtualDHT11Device(ecosystem_uid=self.ecosystem.uid)
+        return VirtualDHT11Device(virtual_ecosystem=self.virtual_ecosystem)
 
 
 class virtualDHT22(virtualDHT):
@@ -49,7 +48,7 @@ class virtualDHT22(virtualDHT):
     def _get_device(self) -> VirtualDHT22Device:
         from gaia.hardware.sensors._devices.virtual import VirtualDHT22Device
 
-        return VirtualDHT22Device(ecosystem_uid=self.ecosystem.uid)
+        return VirtualDHT22Device(virtual_ecosystem=self.virtual_ecosystem)
 
 
 class virtualAHT20(AHT20, virtualSensor):
@@ -58,7 +57,7 @@ class virtualAHT20(AHT20, virtualSensor):
     def _get_device(self) -> VirtualAHTx0Device:
         from gaia.hardware.sensors._devices.virtual import VirtualAHTx0Device
 
-        return VirtualAHTx0Device(ecosystem_uid=self.ecosystem.uid)
+        return VirtualAHTx0Device(virtual_ecosystem=self.virtual_ecosystem)
 
 
 class virtualVCNL4040(VCNL4040, virtualSensor):
@@ -67,7 +66,7 @@ class virtualVCNL4040(VCNL4040, virtualSensor):
     def _get_device(self) -> VirtualVCNL4040Device:
         from gaia.hardware.sensors._devices.virtual import VirtualVCNL4040Device
 
-        return VirtualVCNL4040Device(ecosystem_uid=self.ecosystem.uid)
+        return VirtualVCNL4040Device(virtual_ecosystem=self.virtual_ecosystem)
 
 
 class virtualVEML7700(VEML7700, virtualSensor):
@@ -76,7 +75,7 @@ class virtualVEML7700(VEML7700, virtualSensor):
     def _get_device(self) -> VirtualVEML7700Device:
         from gaia.hardware.sensors._devices.virtual import VirtualVEML7700Device
 
-        return VirtualVEML7700Device(ecosystem_uid=self.ecosystem.uid)
+        return VirtualVEML7700Device(virtual_ecosystem=self.virtual_ecosystem)
 
 
 class virtualCapacitiveMoisture(CapacitiveMoisture, virtualSensor):
@@ -85,7 +84,7 @@ class virtualCapacitiveMoisture(CapacitiveMoisture, virtualSensor):
     def _get_device(self) -> VirtualSeesawDevice:
         from gaia.hardware.sensors._devices.virtual import VirtualSeesawDevice
 
-        return VirtualSeesawDevice(ecosystem_uid=self.ecosystem.uid)
+        return VirtualSeesawDevice(virtual_ecosystem=self.virtual_ecosystem)
 
 
 class virtualENS160(ENS160, virtualSensor):

src/gaia/hardware/virtual.py
@@ -14,14 +14,12 @@ class VirtualDevice:
     def __init__(
             self,
             *args,
-            ecosystem_uid: str,
+            virtual_ecosystem: VirtualEcosystem,
             **kwargs,
     ) -> None:
         super().__init__(*args, **kwargs)
 
-        from gaia.virtual import VirtualEcosystem
-
-        self.virtual_ecosystem = VirtualEcosystem.get(ecosystem_uid)
+        self.virtual_ecosystem = virtual_ecosystem
 
 
 class virtualHardware:
@@ -48,4 +46,4 @@ class virtualHardware:
         super().__init__(*args, **kwargs)
 
         # Will raise if no corresponding VirtualEcosystem instance exists
-        VirtualEcosystem.get(self.ecosystem.uid)
+        self.virtual_ecosystem = VirtualEcosystem.get(self.ecosystem.uid)

src/gaia/hardware/abc.py
@@ -1012,7 +1012,7 @@ class Dimmer(Actuator):
             "This method must be implemented in a subclass"
         )  # pragma: no cover
 
-    async def get_pwm_level(self) -> float:
+    async def get_pwm_level(self) -> int | float:
         raise NotImplementedError("This method must be implemented in a subclass")
 
 

src/gaia/hardware/actuators/GPIO.py
@@ -80,10 +80,10 @@ class gpioDimmer(gpioAddressMixin, Dimmer):
         # Allow a 0.5% tolerance
         return isclose(self.dimmer.duty_cycle, duty_cycle_in_16_bit, rel_tol=0.005)
 
-    async def get_pwm_level(self) -> float | int:
+    async def get_pwm_level(self) -> int | float:
         return await run_sync(self._get_pwm_level)
 
-    def _get_pwm_level(self) -> float | int:
+    def _get_pwm_level(self) -> int | float:
         duty_cycle_in_16_bit = self.dimmer.duty_cycle
         return duty_cycle_in_16_bit / (2**16 - 1) * 100
 

src/gaia/hardware/actuators/websocket.py
@@ -43,7 +43,7 @@ class WebSocketDimmer(WebSocketAddressMixin, Dimmer):
         except (ConnectionError, DeviceError):
             return False
 
-    async def get_pwm_level(self) -> float:
+    async def get_pwm_level(self) -> int | float:
         try:
             return await self._execute_action(
                 {"action": "get_pwm_level"},

commit 38c4358d412edcc3234b012b4787bf1e6ee1d823

Fix simulation issues in `VirtualEcosystem._measure()` introduced by #419

src/gaia/virtual.py
@@ -377,6 +377,7 @@ class VirtualEcosystem:
         heat_quantity += get_output(temp_inc_group, self._max_heater_output, 1.00)
         temp_dec_group = actuators_mapping[(gv.ClimateParameter.temperature, "decrease")]
         heat_quantity -= get_output(temp_dec_group, self._max_heater_output, 0.60)
+        self._heat_quantity = heat_quantity
 
         # Humidity calculation
         d_hum = self.absolute_humidity - get_absolute_humidity(out_temp, out_hum)
@@ -387,11 +388,12 @@ class VirtualEcosystem:
         humidity_quantity += get_output(hum_inc_group, self._max_humidifier_output, 1.00)
         hum_dec_group = actuators_mapping[(gv.ClimateParameter.humidity, "decrease")]
         humidity_quantity -= get_output(hum_dec_group, self._max_humidifier_output, 0.50)
+        self._humidity_quantity = humidity_quantity
 
         # Light calculation
         self._light = out_light
         light_inc_group = actuators_mapping[(gv.ClimateParameter.light, "increase")]
-        self._light += get_output(light_inc_group, self._max_light_output, 1.00)
+        self._light += get_output(light_inc_group, self._max_light_output, 1/d_sec)
 
         # Update
         self._last_update = now