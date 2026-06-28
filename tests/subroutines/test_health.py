30d3de8463ea7aa8dc9b818a525743a49019ade6
Valentin Ambroise <valentin.ambroise@outlook.com>
2026-06-28 22:58:01 +0200

    Use class-based tests for health subroutine

--- tests/subroutines/test_health.py
+++ tests/subroutines/test_health.py
@@ -10,79 +10,78 @@ from ..data import camera_uid
 
 
 @pytest.mark.asyncio
-async def test_manageable(ecosystem: Ecosystem, health_subroutine: Health):
-    camera_cfg = health_subroutine.config.hardware_dict[camera_uid].copy()
-    assert health_subroutine.manageable
+class TestHealthSubroutine:
+    async def test_manageable(self, ecosystem: Ecosystem, health_subroutine: Health):
+        camera_cfg = health_subroutine.config.hardware_dict[camera_uid].copy()
+        assert health_subroutine.manageable
 
-    health_subroutine.config.hardware_dict[camera_uid]["measures"] = []
-    await ecosystem.refresh_hardware()
-    assert not health_subroutine.manageable
+        health_subroutine.config.hardware_dict[camera_uid]["measures"] = []
+        await ecosystem.refresh_hardware()
+        assert not health_subroutine.manageable
 
-    health_subroutine.config.hardware_dict[camera_uid] = camera_cfg
-    await ecosystem.refresh_hardware()
-    assert health_subroutine.manageable
+        health_subroutine.config.hardware_dict[camera_uid] = camera_cfg
+        await ecosystem.refresh_hardware()
+        assert health_subroutine.manageable
 
-    health_subroutine.config.delete_hardware(camera_uid)
-    await ecosystem.refresh_hardware()
-    assert not health_subroutine.manageable
+        health_subroutine.config.delete_hardware(camera_uid)
+        await ecosystem.refresh_hardware()
+        assert not health_subroutine.manageable
 
+    async def test_hardware_needed(self, health_subroutine: Health):
+        uids = health_subroutine.get_hardware_needed_uid()
+        assert uids == {camera_uid}
 
-def test_hardware_needed(health_subroutine: Health):
-    uids = health_subroutine.get_hardware_needed_uid()
-    assert uids == {camera_uid}
+    async def test_routine(self, health_subroutine: Health):
+        # Enable the subroutine
+        health_subroutine.enable()
 
+        # Test start, routine, refresh and stop
+        await health_subroutine.start()
 
-@pytest.mark.asyncio
-async def test_routine(health_subroutine: Health):
-    # Enable the subroutine
-    health_subroutine.enable()
+        assert health_subroutine.plants_health == gv.Empty()
 
-    # Test start, routine, refresh and stop
-    await health_subroutine.start()
+        await health_subroutine.routine()
 
-    assert health_subroutine.plants_health == gv.Empty()
+        assert health_subroutine.plants_health != gv.Empty()
+        assert isinstance(health_subroutine.plants_health.records[0], gv.HealthRecord)
 
-    await health_subroutine.routine()
+        await health_subroutine.refresh()
 
-    assert health_subroutine.plants_health != gv.Empty()
-    assert isinstance(health_subroutine.plants_health.records[0], gv.HealthRecord)
+        await health_subroutine.stop()
 
-    await health_subroutine.refresh()
+        # Disable the subroutine
+        health_subroutine.disable()
 
-    await health_subroutine.stop()
+    async def test_light_switching(
+            self,
+            health_subroutine: Health,
+            light_subroutine: Light,
+            logs_content
+    ):
+        light_subroutine.enable()
+        await light_subroutine.start()
 
-    # Disable the subroutine
-    health_subroutine.disable()
+        health_subroutine.enable()
+        await health_subroutine.start()
 
+        with logs_content():
+            pass  # Clear logs
 
-@pytest.mark.asyncio
-async def test_light_switching(health_subroutine: Health, light_subroutine: Light, logs_content):
-    light_subroutine.enable()
-    await light_subroutine.start()
+        await health_subroutine._get_the_images()
 
-    health_subroutine.enable()
-    await health_subroutine.start()
+        with logs_content() as logs:
+            assert "Light has been set to 'manual' mode" in logs
+            assert "Light has been set to 'automatic' mode" in logs
 
-    with logs_content():
-        pass  # Clear logs
+        await light_subroutine.stop()
+        light_subroutine.disable()
 
-    await health_subroutine._get_the_images()
+    async def test_index(self, health_subroutine: Health):
+        health_subroutine.enable()
+        await health_subroutine.start()
 
-    with logs_content() as logs:
-        assert "Light has been set to 'manual' mode" in logs
-        assert "Light has been set to 'automatic' mode" in logs
+        camera = health_subroutine.hardware[camera_uid]
 
-    await light_subroutine.stop()
-    light_subroutine.disable()
-
-
-@pytest.mark.asyncio
-async def test_index(health_subroutine: Health):
-    health_subroutine.enable()
-    await health_subroutine.start()
-
-    camera = health_subroutine.hardware[camera_uid]
-
-    image = await camera.get_image()
-    index = health_subroutine._get_index(image, Measure.mpri)
-    assert isinstance(index, float)
+        image = await camera.get_image()
+        index = health_subroutine._get_index(image, Measure.mpri)
+        assert isinstance(index, float)
