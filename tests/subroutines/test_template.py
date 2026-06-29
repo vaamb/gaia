794ae15229c3653fa38217c37d2dfc298e72a846
Valentin Ambroise <valentin.ambroise@outlook.com>
2026-06-29 22:30:24 +0200

    Use `caplog` in `TestSubroutineTemplate`

--- tests/subroutines/test_template.py
+++ tests/subroutines/test_template.py
@@ -22,7 +22,11 @@ class TestSubroutineTemplate:
         # Ensure `subroutine_names` and `subroutine_dict` keys remain identical
         assert sorted(subroutine_names) == sorted(subroutine_dict.keys())
 
-    async def test_states(self, dummy_subroutine: Dummy, logs_content):
+    async def test_states(
+            self,
+            dummy_subroutine: Dummy,
+            caplog: pytest.LogCaptureFixture,
+    ):
         dummy_subroutine.manageable_state = False
 
         with pytest.raises(RuntimeError, match=r"The subroutine is not enabled."):
@@ -31,50 +35,54 @@ class TestSubroutineTemplate:
             await dummy_subroutine.stop()
         assert not dummy_subroutine.enabled
 
+        caplog.clear()
         dummy_subroutine.enable()
         assert dummy_subroutine.enabled
-        with logs_content() as logs:
-            assert "Enabling the subroutine." in logs
+        assert "Enabling the subroutine." in caplog.messages
         with pytest.raises(RuntimeError, match=r"The subroutine is not manageable."):
             await dummy_subroutine.start()
 
+        caplog.clear()
         dummy_subroutine.manageable_state = True
         await dummy_subroutine.start()
-        with logs_content() as logs:
-            assert "Starting the subroutine." in logs
+        assert "Starting the subroutine." in caplog.messages
         with pytest.raises(RuntimeError, match=r"The subroutine is already running."):
             await dummy_subroutine.start()
 
+        caplog.clear()
         await dummy_subroutine.stop()
-        with logs_content() as logs:
-            assert "Stopping the subroutine." in logs
+        assert "Stopping the subroutine." in caplog.messages
 
+        caplog.clear()
         dummy_subroutine.disable()
-        with logs_content() as logs:
-            assert "Disabling the subroutine." in logs
+        assert "Disabling the subroutine." in caplog.messages
         assert not dummy_subroutine.enabled
 
-    async def test_start_and_stop_failures(self, dummy_subroutine: Dummy, logs_content):
+    async def test_start_and_stop_failures(
+            self,
+            dummy_subroutine: Dummy,
+            caplog: pytest.LogCaptureFixture,
+    ):
         dummy_subroutine.enable()
 
         # A failing `_start()` should be logged, re-raised and leave the
         # subroutine stopped
+        caplog.clear()
         with patch.object(dummy_subroutine, "_start", side_effect=RuntimeError("Oops")):
             with pytest.raises(RuntimeError, match=r"Oops"):
                 await dummy_subroutine.start()
-        with logs_content() as logs:
-            assert "Starting failed." in logs
+        assert "Starting failed." in caplog.messages[2]
         assert not dummy_subroutine.started
 
         await dummy_subroutine.start()
 
         # A failing `_stop()` should be logged, re-raised and leave the
         # subroutine started
+        caplog.clear()
         with patch.object(dummy_subroutine, "_stop", side_effect=RuntimeError("Oops")):
             with pytest.raises(RuntimeError, match=r"Oops"):
                 await dummy_subroutine.stop()
-        with logs_content() as logs:
-            assert "Stopping failed." in logs
+        assert "Stopping failed." in caplog.messages[1]
         assert dummy_subroutine.started
 
         await dummy_subroutine.stop()
